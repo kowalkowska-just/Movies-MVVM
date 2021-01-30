a8468a783fe23d7701d2c2cc5f07792f7e51a6a0
Justyna <kowalkowska.just@gmail.com>
2021-01-30 18:54:47 +0100

    Setting up Navigation Controller in SceneDelegate and creating TableView.

--- TopMovies-MVVM/TopMovies-MVVM/Controller/TopMoviesViewController.swift
+++ TopMovies-MVVM/TopMovies-MVVM/Controller/TopMoviesViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class TopMoviesViewController: UIViewController {
 
+    //MARK: - Properties
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .darkGray
+        
+        setupTableView()
+        setupNavigationBar()
     }
 
+    //MARK: - Helper Functions
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func setupNavigationBar() {
+        navigationItem.title = "TOP MOVIES"
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.backgroundColor = .darkGray
+        navigationController?.navigationBar.tintColor = .white
 
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+//MARK: - Extension UITableViewDelegate and UITableViewDataSource
+
+extension TopMoviesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Justyna"
+        return cell
+    }
 }
 
