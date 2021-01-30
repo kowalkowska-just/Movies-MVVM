6dc7e4dc834e4e424fccbcaa6e30bb516ccc48f8
Justyna <kowalkowska.just@gmail.com>
2021-01-30 17:45:35 +0100

    Setting up project in Xcode.

---
+++ TopMovies-MVVM/TopMovies-MVVM/Controller/TopMoviesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TopMoviesViewController.swift
+//  TopMovies-MVVM
+//
+//  Created by Justyna Kowalkowska on 30/01/2021.
+//
+
+import UIKit
+
+class TopMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
